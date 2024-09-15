32a600810299786d9cb4446667b0fd2173a3a684
=empiretylh <=thuralinhtut3@gmail.com>
2024-09-15 23:18:39 +0630

    Admin Web domain

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    host : '0.0.0.0',
-    port : 3000,
+  base: '/Admin2DWeb/', // Replace 'your-repo-name' with the name of your GitHub repository
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
   }
 })
